b6f7726f7439d2ae1b5fc947a5a5166dcfeb98c7
Pouya <pouya.t.003@gmail.com>
2025-12-31 23:59:21 +0330

    refactor: write tags in camelCase

--- internal/domain/user.go
+++ internal/domain/user.go
@@ -5,9 +5,9 @@ import "time"
 type User struct {
 	ID                 int64     `json:"id" db:"id"`
 	Handle             string    `json:"handle" db:"handle"`
-	CurrentStreak      int       `json:"current_streak" db:"current_streak"`
-	MaxStreak          int       `json:"max_streak" db:"max_streak"`
-	LastSubmissionDate time.Time `json:"last_submission_date" db:"last_submission_date"`
-	LastUpdatedAt      time.Time `json:"last_updated_at" db:"last_updated_at"`
-	CreatedAt          time.Time `json:"created_at" db:"created_at"`
+	CurrentStreak      int       `json:"currentStreak" db:"currentStreak"`
+	MaxStreak          int       `json:"maxStreak" db:"maxStreak"`
+	LastSubmissionDate time.Time `json:"lastSubmissionDate" db:"lastSubmissionDate"`
+	LastUpdatedAt      time.Time `json:"lastUpdatedAt" db:"lastUpdatedAt"`
+	CreatedAt          time.Time `json:"createdAt" db:"createdAt"`
 }
