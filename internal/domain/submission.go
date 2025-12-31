164196195b9617e9ed1cc6c3834583dbe88371ad
Pouya <pouya.t.003@gmail.com>
2026-01-01 00:09:57 +0330

    feat: add getDate function

--- internal/domain/submission.go
+++ internal/domain/submission.go
@@ -1,6 +1,15 @@
 package domain
 
+import "time"
+
 type Submission struct {
 	CreationTimeSeconds int64  `json:"creationTimeSeconds" db:"creationTimeSeconds"`
 	Verdict             string `json:"verdict" db:"verdict"`
+}
+
+func (s *Submission) GetSubmissionDate() time.Time {
+	utcTime := time.Unix(s.CreationTimeSeconds, 0)
+
+	IranTime := 3*time.Hour + 30*time.Minute
+	return utcTime.Add(IranTime)
 }
