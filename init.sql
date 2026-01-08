b0586ac3eb57b642a679fc2123b0ea76cdb5ba9d
Reza-namvaran <reza.namvaran823@gmail.com>
2026-01-08 16:28:15 +0330

    (refactor): Update init.sql

--- init.sql
+++ init.sql
@@ -1,10 +1,3 @@
--- Create database if it doesn't exist
-SELECT 'CREATE DATABASE "CodeStreaks"'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'CodeStreaks')\gexec
-
--- Connect to the database
-\c CodeStreaks
-
 -- Create extensions if needed
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
